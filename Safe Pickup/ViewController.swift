bc878f9d652a5ac9d8ed0cc79baa9a637779a224
Andres Sada <andres.sada@gmail.com>
2018-02-28 18:26:18 -0600

    Initial Commit

---
+++ Safe Pickup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Safe Pickup
+//
+//  Created by Andres Luis Sada Govela on 28/02/18.
+//  Copyright © 2018 Andres Luis Sada Govela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
